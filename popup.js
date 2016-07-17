64459ad74be771d169afed475db12b3b94149509
ArmandoAssuncao <armando.assuncao.93@gmail.com>
2016-07-16 21:54:47 -0300

    0.12.0 change UI and locales

--- popup.js
+++ popup.js
@@ -7,67 +7,112 @@ chrome.runtime.getBackgroundPage(function(window){
 
 
 document.addEventListener('DOMContentLoaded', function () {
+    //input types
+    $('.type_url > a').text(chrome.i18n.getMessage('input_type_url'));
+    $('.type_file > a').text(chrome.i18n.getMessage('input_type_file'));
+    $('.type_color > a').text(chrome.i18n.getMessage('input_type_color'));
+
+
     // background
     var btnAddBg = $('#btn_add_bg');
-    var btnRemoveBg = $('#btn_del_bg');
-    var valueBg = $('#value_bg');
     var btnTypeBg = $('#btn_type_bg');
-    btnAddBg.text(chrome.i18n.getMessage('btn_change_bg'));
-    valueBg.prop('placeholder', chrome.i18n.getMessage('textfield_holder'));
-    valueBg.prop('title', chrome.i18n.getMessage('textfield_msg_erro'));
+    var valueBg = $('#value_bg');
+    var btnRemoveBg = $('#btn_del_bg');
+    btnAddBg.text(chrome.i18n.getMessage('global_btn_add_bg'));
+    btnAddBg.prop('title', chrome.i18n.getMessage('global_title_btn_add_bg'));
+    btnTypeBg.html(chrome.i18n.getMessage('input_type_url') + ' <span class="caret"></span>');
+    valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_url'));
+    btnRemoveBg.prop('title', chrome.i18n.getMessage('global_title_btn_remove_bg'));
 
     // background top
-    var btnAddBgTop = $('#btn_add_bg_top');
-    var btnRemoveBgTop = $('#btn_del_bg_top');
-    var valueBgTop = $('#value_bg_top');
+    /*var btnRemoveBgTop = $('#btn_del_bg_top');
+    var valueBgTop = $('#value_bg_top');*/
 
     //backgrond chat
     var btnAddBgChat = $('#btn_add_bg_chat');
-    var btnRemoveBgChat = $('#btn_del_bg_chat');
-    var valueBgChat = $('#value_bg_chat');
     var btnTypeBgChat = $('#btn_type_bg_chat');
+    var valueBgChat = $('#value_bg_chat');
+    var btnRemoveBgChat = $('#btn_del_bg_chat');
+    btnAddBgChat.text(chrome.i18n.getMessage('chat_btn_add_bg'));
+    btnAddBg.prop('title', chrome.i18n.getMessage('chat_title_btn_add_bg'));
+    btnTypeBgChat.html(chrome.i18n.getMessage('input_type_url') + ' <span class="caret"></span>');
+    valueBgChat.prop('placeholder', chrome.i18n.getMessage('placeholder_input_url'));
+    btnRemoveBgChat.prop('title', chrome.i18n.getMessage('chat_title_btn_remove_bg'));
 
     // panel header
-    var btnAddBgPanelHeader = $('#btn_add_bg_panelheader');
-    var btnRemoveBgPanelHeader = $('#btn_del_bg_panelheader');
+    var legendPanel = $('#legend_panel');
+    var nameInputPanel = $('#name_input_panel');
     var valueBgPanelHeader = $('#value_bg_panelheader');
-
-    // panel message
-    var btnAddBgPanelMessage = $('#btn_add_bg_panelmessage');
-    var btnRemoveBgPanelMessage = $('#btn_del_bg_panelmessage');
-    var valueBgPanelMessage = $('#value_bg_panelmessage');
-
-    // message in
-    var btnAddBgMessageIn = $('#btn_add_bg_messagein');
-    var btnRemoveBgMessageIn = $('#btn_del_bg_messagein');
-    var valueBgMessageIn = $('#value_bg_messagein');
-
-    // message out
-    var btnAddBgMessageOut = $('#btn_add_bg_messageout');
-    var btnRemoveBgMessageOut = $('#btn_del_bg_messageout');
-    var valueBgMessageOut = $('#value_bg_messageout');
-
-    // text message in
-    var btnAddTextMessageIn = $('#btn_add_text_messagein');
-    var btnRemoveTextMessageIn = $('#btn_del_text_messagein');
-    var valueTextMessageIn = $('#value_text_messagein');
-
-    // text message out
-    var btnAddTextMessageOut = $('#btn_add_text_messageout');
-    var btnRemoveTextMessageOut = $('#btn_del_text_messageout');
-    var valueTextMessageOut = $('#value_text_messageout');
+    var btnRemoveBgPanelHeader = $('#btn_del_bg_panelheader');
+    legendPanel.text(chrome.i18n.getMessage('panel_fieldset_legend'));
+    nameInputPanel.text(chrome.i18n.getMessage('panel_name_input_bg'));
+    valueBgPanelHeader.prop('placeholder', chrome.i18n.getMessage('panel_placeholder_input_bg'));
+    valueBgPanelHeader.prop('title', chrome.i18n.getMessage('panel_title_input_bg'));
+    btnRemoveBgPanelHeader.prop('title', chrome.i18n.getMessage('panel_title_btn_remove_bg'));
 
     // chat title
-    var btnAddChatTitle = $('#btn_add_chat_title');
-    var btnRemoveChatTitle = $('#btn_del_chat_title');
-    var valueChatTitle = $('#value_chat_title');
+    var nameInputChatTitle = $('#name_input_chattitle');
+    var valueChatTitle = $('#value_chattitle');
+    var btnRemoveChatTitle = $('#btn_del_chattitle');
+    nameInputChatTitle.text(chrome.i18n.getMessage('chattitle_name_input_textcolor'));
+    valueChatTitle.prop('placeholder', chrome.i18n.getMessage('chattitle_placeholder_input_textcolor'));
+    valueChatTitle.prop('title', chrome.i18n.getMessage('chattitle_title_input_textcolor'));
+    btnRemoveChatTitle.prop('title', chrome.i18n.getMessage('chattitle_title_btn_remove_textcolor'));
+    
+    // panel message
+    /*var btnAddBgPanelMessage = $('#btn_add_bg_panelmessage');
+    var btnRemoveBgPanelMessage = $('#btn_del_bg_panelmessage');
+    var valueBgPanelMessage = $('#value_bg_panelmessage');*/
 
-    //delete config
-    var btnDeleteConfig = $('#btn_delete_config');
+    // message out
+    var legendMessageOut = $('#legend_messageout');
+    var nameInputMessageOut = $('#name_input_messageout');
+    var valueBgMessageOut = $('#value_bg_messageout');
+    var btnRemoveBgMessageOut = $('#btn_del_bg_messageout');
+    legendMessageOut.text(chrome.i18n.getMessage('messageout_fieldset_legend'));
+    nameInputMessageOut.text(chrome.i18n.getMessage('messageout_name_input_bg'));
+    valueBgMessageOut.prop('placeholder', chrome.i18n.getMessage('messageout_placeholder_input_bg'));
+    valueBgMessageOut.prop('title', chrome.i18n.getMessage('messageout_title_input_bg'));
+    btnRemoveBgMessageOut.prop('title', chrome.i18n.getMessage('messageout_title_btn_remove_bg'));
+
+
+    // text message out
+    var nameInputTextMessageOut = $('#name_input_text_messageout');
+    var valueTextMessageOut = $('#value_text_messageout');
+    var btnRemoveTextMessageOut = $('#btn_del_text_messageout');
+    nameInputTextMessageOut.text(chrome.i18n.getMessage('textmessageout_name_input_textcolor'));
+    valueTextMessageOut.prop('placeholder', chrome.i18n.getMessage('textmessageout_placeholder_input_textcolor'));
+    valueTextMessageOut.prop('title', chrome.i18n.getMessage('textmessageout_title_input_textcolor'));
+    btnRemoveTextMessageOut.prop('title', chrome.i18n.getMessage('textmessageout_title_btn_remove_textcolor'));
+
+    // message in
+    var legendMessageIn = $('#legend_messagein');
+    var nameInputMessageIn = $('#name_input_messagein');
+    var valueBgMessageIn = $('#value_bg_messagein');
+    var btnRemoveBgMessageIn = $('#btn_del_bg_messagein');
+    legendMessageIn.text(chrome.i18n.getMessage('messagein_fieldset_legend'));
+    nameInputMessageIn.text(chrome.i18n.getMessage('messagein_name_input_bg'));
+    valueBgMessageIn.prop('placeholder', chrome.i18n.getMessage('messagein_placeholder_input_bg'));
+    valueBgMessageIn.prop('title', chrome.i18n.getMessage('messagein_title_input_bg'));
+    btnRemoveBgMessageIn.prop('title', chrome.i18n.getMessage('messagein_title_btn_remove_bg'));
+
+    // text message in
+    var nameInputTextMessageIn = $('#name_input_text_messagein');
+    var valueTextMessageIn = $('#value_text_messagein');
+    var btnRemoveTextMessageIn = $('#btn_del_text_messagein');
+    nameInputTextMessageIn.text(chrome.i18n.getMessage('textmessagein_name_input_textcolor'));
+    valueTextMessageIn.prop('placeholder', chrome.i18n.getMessage('textmessagein_placeholder_input_textcolor'));
+    valueTextMessageIn.prop('title', chrome.i18n.getMessage('textmessagein_title_input_textcolor'));
+    btnRemoveTextMessageIn.prop('title', chrome.i18n.getMessage('textmessagein_title_btn_remove_textcolor'));
+
+    //restore default
+    var btnRestoreDefault = $('#btn_delete_config');
+    btnRestoreDefault.text(chrome.i18n.getMessage('restoredefault_btn'));
+    btnRestoreDefault.prop('title', chrome.i18n.getMessage('restoredefault_title'));
 
 
     //bg global
-    btnAddBg.on('click.type_link', function(){
+    btnAddBg.on('click.type_url', function(){
         funcAdd(constants.BG_GLOBAL.add, {imgUrl: valueBg.val()});
     });
     $('#type_bg li').click(function(){
@@ -75,6 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
         valueBg.val('');
 
         if($(this).hasClass('type_color')){
+            valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_color'));
             valueBg.prop('type', 'text');
             btnAddBg.prop('disabled', true);
             valueBg.colorpicker().on('changeColor', function(e) {
@@ -84,15 +130,17 @@ document.addEventListener('DOMContentLoaded', function () {
         else{
             if(valueBg.hasClass('colorpicker-element')) valueBg.colorpicker('destroy');
             btnAddBg.prop('disabled', false);
-            btnAddBg.off('click.type_file').off('click.type_link');
+            btnAddBg.off('click.type_file').off('click.type_url');
 
-            if($(this).hasClass('type_link')){
+            if($(this).hasClass('type_url')){
+                valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_url'));
                 valueBg.prop('type', 'url');
-                btnAddBg.on('click.type_link', function(){
+                btnAddBg.on('click.type_url', function(){
                     funcAdd(constants.BG_GLOBAL.add, {imgUrl: valueBg.val()});
                 });
             }
             else if($(this).hasClass('type_file')){
+                valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_file'));
                 valueBg.prop('type', 'file');
                 btnAddBg.on('click.type_file', function(){
                     readLocalImg(valueBg[0], function(imgB64){
@@ -104,14 +152,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     //bg chat
-    btnAddBgChat.on('click.type_link', function(){
-        funcAdd(constants.BG_GLOBAL.add, {imgUrl: valueBgChat.val()});
+    btnAddBgChat.on('click.type_url', function(){
+        funcAdd(constants.BG_CHAT.add, {imgUrl: valueBgChat.val()});
     });
     $('#type_bg_chat li').click(function(){
         btnTypeBgChat.html($(this).text() + ' <span class="caret"></span>');
         valueBgChat.val('');
 
         if($(this).hasClass('type_color')){
+            valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_color'));
             valueBgChat.prop('type', 'text');
             btnAddBgChat.prop('disabled', true);
             valueBgChat.colorpicker().on('changeColor', function(e) {
@@ -121,15 +170,17 @@ document.addEventListener('DOMContentLoaded', function () {
         else{
             if(valueBgChat.hasClass('colorpicker-element')) valueBgChat.colorpicker('destroy');
             btnAddBgChat.prop('disabled', false);
-            btnAddBgChat.off('click.type_file').off('click.type_link');
+            btnAddBgChat.off('click.type_file').off('click.type_url');
 
-            if($(this).hasClass('type_link')){
+            if($(this).hasClass('type_url')){
+                valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_url'));
                 valueBgChat.prop('type', 'url');
-                btnAddBgChat.on('click.type_link', function(){
+                btnAddBgChat.on('click.type_url', function(){
                     funcAdd(constants.BG_CHAT.add, {imgUrl: valueBgChat.val()});
                 });
             }
             else if($(this).hasClass('type_file')){
+                valueBg.prop('placeholder', chrome.i18n.getMessage('placeholder_input_file'));
                 valueBgChat.prop('type', 'file');
                 btnAddBgChat.on('click.type_file', function(){
                     readLocalImg(valueBgChat[0], function(imgB64){
@@ -175,7 +226,7 @@ document.addEventListener('DOMContentLoaded', function () {
     btnRemoveTextMessageOut.on('click', function(){ funcRemove(constants.TEXT_MESSAGE_OUT.remove) });
     btnRemoveChatTitle.on('click', function(){ funcRemove(constants.CHAT_TITLE.remove) });
     
-    btnDeleteConfig.on('click', deleteConfig);
+    btnRestoreDefault.on('click', function(){ funcRemove(constants.DELETE_CONFIG) });
 });
 
 function funcAdd(option, obj){
@@ -187,29 +238,28 @@ function funcAdd(option, obj){
 }
 
 function funcRemove(option, obj){
-    var r = confirm(chrome.i18n.getMessage('remover_bg_pergunta'));
+    var r;
+    if(option == constants.DELETE_CONFIG)
+        r = confirm(chrome.i18n.getMessage('question_restore_default'));
+    else
+        r = confirm(chrome.i18n.getMessage('question_remove_item'));
+
     if (r == true) {
         chrome.runtime.getBackgroundPage(function(bg){
             bg.backgroundFunction(option, obj);
+            $('#message_warning').removeClass('_hidden');
         });
-    } else {
-        //nada
     }
 }
 
-function deleteConfig(){
-    chrome.runtime.getBackgroundPage(function(bg){
-        bg.backgroundFunction(constants.DELETE_CONFIG);
-    });
-}
 
 function readLocalImg(element, callback) {
     if (element.files && element.files[0]) {
         if(!element.files[0].type.startsWith('image')){
-            //accept only image
+            alert(chrome.i18n.getMessage('alert_type_file_error'));
         }
         else if(element.files[0].size > 1.5 * 1000000){ // > 1.5MB
-            //accept image less than 1.5MB
+            alert(chrome.i18n.getMessage('alert_size_file_error'));
         }
         else{
             var FR = new FileReader();
